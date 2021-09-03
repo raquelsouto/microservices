71b5de294386dc849161a9f457c2ed9ca50428cc
Raquel Souto <kelsouto@gmail.com>
2021-09-03 16:19:06 -0300

    Project hr-payroll created

--- hr-payroll/src/test/java/com/devsuperior/hrpayroll/HrPayrollApplicationTests.java
+++ hr-payroll/src/test/java/com/devsuperior/hrpayroll/HrPayrollApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.hrworker;
+package com.devsuperior.hrpayroll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrWorkerApplicationTests {
+class HrPayrollApplicationTests {
 
 	@Test
 	void contextLoads() {
