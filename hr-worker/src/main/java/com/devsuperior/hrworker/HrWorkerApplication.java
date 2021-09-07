585c4eb512d103e649d75470bf90c149dc777d16
Raquel Souto <kelsouto@gmail.com>
2021-09-07 20:22:47 -0300

    Configure hr-worker project as a server client

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class HrWorkerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrWorkerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrWorkerApplication.class, args);
+    }
 
 }
--- hr-worker/src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
@@ -1,9 +1,12 @@
 package com.devsuperior.hrworker.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.hrworker.entities.Worker;
 
 public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
 
 }
